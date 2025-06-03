afcf9df12359feca53374858234fb551c9c3b1a3
Emmanuel Guefif <eguefif@fastmail.com>
2025-06-02 21:57:56 -0400

    Read from main interface and print source addr

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use pnet::datalink::Channel::Ethernet;
+use pnet::datalink::{interfaces, ChannelType, Config, NetworkInterface};
+use pnet::packet::ipv4::Ipv4Packet;
+
 fn main() {
-    println!("Hello, world!");
+    if let Some(iface) = get_main_interface() {
+        println!("Starting working on interface: {}", iface.name);
+        let mut config = Config::default();
+        config.channel_type = ChannelType::Layer3(0x800);
+
+        let mut rx = match pnet::datalink::channel(&iface, config) {
+            Ok(Ethernet(_, rx)) => rx,
+            Ok(_) => panic!("Channel format not handled"),
+            Err(e) => panic!("Error: {e}"),
+        };
+        loop {
+            match rx.next() {
+                Ok(packet) => {
+                    let packet = Ipv4Packet::new(packet).unwrap();
+                    let source = packet.get_source();
+                    println!("Source: {}", source);
+                }
+                Err(e) => eprintln!("Error while processing packet: {e}"),
+            }
+        }
+    }
+}
+
+fn get_main_interface() -> Option<NetworkInterface> {
+    let all_interfaces = interfaces();
+    let iface = all_interfaces
+        .iter()
+        .find(|e| e.is_up() && !e.is_loopback() && !e.ips.is_empty());
+    iface.cloned()
 }
