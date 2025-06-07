c337cddea856fcabaaae21189db0da88d9d5b558
Emmanuel Guefif <eguefif@fastmail.com>
2025-06-06 20:47:48 -0400

    Checksum for IP and TCP caculated properly

--- src/packet_crafter.rs
+++ src/packet_crafter.rs
@@ -1,49 +1,43 @@
 use pnet::packet::ip::IpNextHeaderProtocols;
 use pnet::packet::ipv4::MutableIpv4Packet;
 use pnet::packet::tcp::MutableTcpPacket;
-use pnet::packet::Packet;
 use std::net::Ipv4Addr;
 use std::process;
 
-const PORT_SOURCE: u16 = 62355;
+pub const PORT_SOURCE: u16 = 0xa393;
 
 pub fn get_syn_packet(buffer: &mut [u8]) {
-    {
-        let mut ip_packet =
-            MutableIpv4Packet::new(buffer).expect("Impossible to create mutable IP packet");
-        set_ip_packet(&mut ip_packet);
-    }
+    let mut ip_packet =
+        MutableIpv4Packet::new(buffer).expect("Impossible to create mutable IP packet");
+    set_ip_packet(&mut ip_packet);
     let mut tcp_packet =
-        MutableTcpPacket::new(&mut buffer[20..]).expect("Impossible to create mutable tcp packet");
+        MutableTcpPacket::new(&mut buffer[20..]).expect("Impossible to create mutable TCP packet");
     set_tcp_packet(&mut tcp_packet);
-
     let mut ip_packet = MutableIpv4Packet::new(buffer).unwrap();
-    let checksum = pnet::util::checksum(ip_packet.packet(), 0);
     ip_packet.set_checksum(0);
+    let checksum = pnet::packet::ipv4::checksum(&ip_packet.to_immutable());
     ip_packet.set_checksum(checksum);
-    ip_packet.set_total_length(20 + 24);
     println!("buffer {:x?}", &buffer[0..44]);
 }
 
 fn set_ip_packet(packet: &mut MutableIpv4Packet) {
     packet.set_version(4);
-    packet.set_ttl(100);
+    packet.set_ttl(0x35);
     packet.set_next_level_protocol(IpNextHeaderProtocols::Tcp);
     packet.set_identification(process::id() as u16);
     packet.set_header_length(5);
     packet.set_total_length(21);
-    //packet.set_destination(Ipv4Addr::new(127, 0, 0, 1));
-    //packet.set_source(Ipv4Addr::new(127, 0, 0, 1));
     packet.set_source(Ipv4Addr::new(192, 168, 2, 23));
     packet.set_destination(Ipv4Addr::new(192, 168, 2, 1));
+    packet.set_total_length(20 + 24);
 }
 
 fn set_tcp_packet(packet: &mut MutableTcpPacket) {
     packet.set_source(PORT_SOURCE);
-    packet.set_destination(8080);
+    packet.set_destination(80);
     packet.set_data_offset(6);
     packet.set_flags(0b000010);
-    packet.set_sequence(1951641362);
+    packet.set_sequence(0x7453af12);
     packet.set_acknowledgement(0);
     packet.set_window(1024);
 
@@ -54,19 +48,12 @@ fn set_tcp_packet(packet: &mut MutableTcpPacket) {
     };
     packet.set_options(&[max_segment_opt]);
 
+    // FIX: According to wireshark, our checksum is wrong
     packet.set_checksum(0);
-    // TODO: seems that my checksum is wrong. I compared
-    // with the one from a nmap packet captured with the exact
-    // same header.
-    let checksum = pnet::util::ipv4_checksum(
-        &packet.packet(),
-        0,
-        &[],
+    let checksum = pnet::packet::tcp::ipv4_checksum(
+        &packet.to_immutable(),
         &Ipv4Addr::new(192, 168, 2, 23),
         &Ipv4Addr::new(192, 168, 2, 1),
-        IpNextHeaderProtocols::Tcp,
     );
     packet.set_checksum(checksum);
-    println!("Checksum: {:x}", checksum);
-    println!("SHould beChecksum: {:x}", 0xd833);
 }
--- src/syn_scan.rs
+++ src/syn_scan.rs
@@ -1,6 +1,7 @@
 use crate::interface::get_interface;
-use crate::packet_crafter::get_syn_packet;
+use crate::packet_crafter::{get_syn_packet, PORT_SOURCE};
 use crate::Params;
+use pnet::packet::ip::IpNextHeaderProtocols;
 use std::thread;
 
 use pnet::datalink::Channel::Ethernet;
@@ -20,8 +21,13 @@ pub fn run_syn_scan(params: Params) {
         Err(e) => panic!("Error: {e}"),
     };
 
-    thread::spawn(move || listen(rx));
     send(tx);
+    let listener = thread::spawn(move || listen(rx));
+
+    match listener.join() {
+        Ok(_) => println!("Scan is over"),
+        Err(e) => panic!("Error: {e:?}"),
+    }
 }
 
 fn send(mut tx: Box<dyn DataLinkSender>) {
@@ -42,6 +48,9 @@ fn listen(mut rx: Box<dyn DataLinkReceiver + 'static>) {
     loop {
         match rx.next() {
             Ok(packet) => {
+                if should_dismiss_packet(packet) {
+                    continue;
+                }
                 let ip_packet = Ipv4Packet::new(packet).unwrap();
                 let tcp_packet = TcpPacket::new(packet).unwrap();
                 println!("New packet:");
@@ -56,8 +65,20 @@ fn listen(mut rx: Box<dyn DataLinkReceiver + 'static>) {
                     tcp_packet.get_destination()
                 );
                 println!();
+                println!("bytes: {:x?}", packet);
             }
             Err(e) => eprintln!("Error while processing packet: {e}"),
         }
     }
+}
+
+fn should_dismiss_packet(packet: &[u8]) -> bool {
+    let ip_packet = Ipv4Packet::new(packet).unwrap();
+    let tcp_packet = TcpPacket::new(packet).unwrap();
+    if ip_packet.get_next_level_protocol() != IpNextHeaderProtocols::Tcp
+        || tcp_packet.get_destination() != PORT_SOURCE
+    {
+        return true;
+    }
+    false
 }
