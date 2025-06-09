473fbdde734fb16dabc47b4541ef0c1c845d3b77
Emmanuel Guefif <eguefif@fastmail.com>
2025-06-08 21:09:26 -0400

    Update run sh to setcap instead of using sudo

--- run.sh
+++ run.sh
@@ -1,3 +1,4 @@
 #/bin/bash
 cargo build
-sudo ./target/debug/ft_nmap -sS -t 192.168.2.1 -p 80-82,123,144
+sudo setcap  CAP_NET_RAW+eip ./target/debug/ft_nmap 
+./target/debug/ft_nmap -sS -t 192.168.2.1 -p 80-82,123,144
